f61ffabef11659235be2e5ecf71184af8c759b7e
math <master_of_puppets57@hotmail.fr>
2017-09-28 10:16:37 +0200

    rajouter des dépendances dans le head

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,7 +2,9 @@
 <html ng-app="app">
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="app/css/custom.css">
+		 <!-- Stylesheets -->
+		<link rel="stylesheet" href="css/custom.css">
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
@@ -11,7 +13,7 @@
 		<script type="text/javascript" src="<?php echo url('app/node_modules/angular-route/angular-route.min.js') ?>"></script>
 		<script type="text/javascript" src="<?php echo url('app/app.js') ?>"></script>
 		<script type="text/javascript" src="<?php echo url('app/routing.js') ?>"></script>
-		<script type="text/javascript" src="<?php echo url('app/controllers/accueilController.js') ?>"></script>
+		<script type="text/javascript" src="<?php echo url('app/controllers/loginController.js') ?>"></script>
 		
 		<title>annuaire ifa</title>
 	</head>
