972dc20605a76226db356be9aabb37a7b9a45e04
johan <johan.prugnieres@gmail.com>
2017-09-28 11:29:03 +0200

    modification template accueil

--- public/app/views/accueil.php
+++ public/app/views/accueil.php
@@ -1,9 +1,87 @@
 <div class="container-fluid">
 
-    <div class="row justify-content-center">
+    <div class="row">
+        
+        <div class="col-4 bg-secondary d-flex flex-column">
+            <div class="row justify-content-center">
+                <h1>Critères de recherche</h1>
+            </div>
+            <form class="">
+                <div class="form-group">
+                    <input type="nom" class="form-control" id="nom" placeholder="Nom">
+                </div>
+                <div class="form-group">
+                    <input type="prenom" class="form-control" id="exampleInputPrenom1" placeholder="Prénom">
+                </div>
+                <div class="form-group">
+                    <select class="form-control" id="selectFormation">
+                    <option>BTS</option>
+                    <option>DUT</option>
+                    <option>MASTER</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" id="selectAnnee">
+                    <option>20017</option>
+                    <option>2016</option>
+                    <option>2015</option>
+                    </select>
+                </div>
+            </form>
+            <button type="submit" class="btn btn-warning btn-block ">Rechercher</button>
+        
+        </div>
+        <div class="col-8">
+            <table class="table">
+                <thead class="table-inverse">
+                    <tr>
+                    <th>#</th>
+                    <th>Elève</th>
+                    <th>E-mail</th>
+                    <th>Formation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     
-        <h1>Page d'accueil</h1>
-
     </div>
 
 </div>
