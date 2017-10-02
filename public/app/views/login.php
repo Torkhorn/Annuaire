a61ae056e1bf517090c868146d104a403591e069
math <master_of_puppets57@hotmail.fr>
2017-10-02 10:12:37 +0200

    correction indentation

--- public/app/views/login.php
+++ public/app/views/login.php
@@ -24,77 +24,77 @@
 
 
 <div class="container">
-<form class="form-horizontal" role="form" method="POST" action="/login">
-<div class="row">
-	<div class="col-md-3"></div>
-	<div class="col-md-6">
-		<h2>Please Login</h2>
-		<hr>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-3"></div>
-	<div class="col-md-6">
-		<div class="form-group has-danger">
-			<label class="sr-only" for="email">E-Mail Address</label>
-			<div class="input-group mb-2 mr-sm-2 mb-sm-0">
-				<div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
-				<input type="text" name="email" class="form-control" id="email"
-						placeholder="you@example.com" required autofocus>
+	<form class="form-horizontal" role="form" method="POST" action="/login">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<h2>Please Login</h2>
+				<hr>
 			</div>
 		</div>
-	</div>
-	<!-- colonne message d'erreur -->
-	<div class="col-md-3">
-		<div class="form-control-feedback">
-			<span class="text-danger align-middle">
-				<!-- mettre le message d'erreur d'identifiant
-				<i class="fa fa-close"></i> Example error message -->
-			</span>
-		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-3"></div>
-	<div class="col-md-6">
-		<div class="form-group">
-			<label class="sr-only" for="password">Password</label>
-			<div class="input-group mb-2 mr-sm-2 mb-sm-0">
-				<div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
-				<input type="password" name="password" class="form-control" id="password"
-						placeholder="Password" required>
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<div class="form-group has-danger">
+					<label class="sr-only" for="email">E-Mail Address</label>
+					<div class="input-group mb-2 mr-sm-2 mb-sm-0">
+						<div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-at"></i></div>
+						<input type="text" name="email" class="form-control" id="email"
+								placeholder="you@example.com" required autofocus>
+					</div>
+				</div>
+			</div>
+			<!-- colonne message d'erreur -->
+			<div class="col-md-3">
+				<div class="form-control-feedback">
+					<span class="text-danger align-middle">
+						<!-- mettre le message d'erreur d'identifiant
+						<i class="fa fa-close"></i> Example error message -->
+					</span>
+				</div>
 			</div>
 		</div>
-	</div>
-	<!-- colonne message d'erreur  -->
-	<div class="col-md-3">
-		<div class="form-control-feedback">
-			<span class="text-danger align-middle">
-			<!-- Put password error message here -->    
-			</span>
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label class="sr-only" for="password">Password</label>
+					<div class="input-group mb-2 mr-sm-2 mb-sm-0">
+						<div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
+						<input type="password" name="password" class="form-control" id="password"
+								placeholder="Password" required>
+					</div>
+				</div>
+			</div>
+			<!-- colonne message d'erreur  -->
+			<div class="col-md-3">
+				<div class="form-control-feedback">
+					<span class="text-danger align-middle">
+					<!-- Put password error message here -->    
+					</span>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-3"></div>
-	<div class="col-md-6" style="padding-top: .35rem">
-		<div class="form-check mb-2 mr-sm-2 mb-sm-0">
-			<label class="form-check-label">
-				<input class="form-check-input" name="remember"
-						type="checkbox" >
-				<span style="padding-bottom: .15rem">Remember me</span>
-			</label>
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-6" style="padding-top: .35rem">
+				<div class="form-check mb-2 mr-sm-2 mb-sm-0">
+					<label class="form-check-label">
+						<input class="form-check-input" name="remember"
+								type="checkbox" >
+						<span style="padding-bottom: .15rem">Remember me</span>
+					</label>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<div class="row" style="padding-top: 1rem">
-	<div class="col-md-3"></div>
-	<div class="col-md-6">
-		<button type="submit" class="btn btn-success"><i class="fa fa-sign-in"></i> Login</button>
-		<a href="javascript:;" class="forget" data-toggle="modal" data-target=".forget-modal">Forgot your password?</a>
-	</div>
-</div>
-</form>
+		<div class="row" style="padding-top: 1rem">
+			<div class="col-md-3"></div>
+			<div class="col-md-6">
+				<button type="submit" class="btn btn-success"><i class="fa fa-sign-in"></i> Login</button>
+				<a href="javascript:;" class="forget" data-toggle="modal" data-target=".forget-modal">Forgot your password?</a>
+			</div>
+		</div>
+		</form>
 </div>
 
 <!-- Fenetre pour l'oubli de mot de passe  -->
