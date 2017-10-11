b04a8a0df934a15d590171cf91322d9c52d2fed7
math <master_of_puppets57@hotmail.fr>
2017-10-11 14:25:57 +0200

    remis la barre de nav en dur

--- public/app/views/login.php
+++ public/app/views/login.php
@@ -9,7 +9,7 @@
 		<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" href="#">Features</a>
+		<a class="nav-link" href="#git stat">Features</a>
 	</li>
 	<li class="nav-item">
 		<a class="nav-link" href="#">Pricing</a>
