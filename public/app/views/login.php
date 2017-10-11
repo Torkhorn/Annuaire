627813ee8f9a059675b1a893c59e72e15ef48d9e
math <master_of_puppets57@hotmail.fr>
2017-10-11 14:30:29 +0200

    modifé la barre de nav de login, et retiré un 'require' de l'index.php

--- public/app/views/login.php
+++ public/app/views/login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <nav class="navbar navbar-dark bg-dark">
 <a class="navbar-brand" href="#">IFA</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,33 +20,6 @@
 	</span>
 </div>
 </nav>
-=======
-<!-- <nav class="navbar navbar-dark bg-dark">
-	<a class="navbar-brand" href="#">IFA</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarText">
-		<ul class="navbar-nav mr-auto">
-		<li class="nav-item active">
-			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Features</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="#">Pricing</a>
-		</li>
-		</ul>
-		<span class="navbar-text">
-		Navbar text with an inline element
-		</span>
-	</div>
-</nav> -->
-
-<my-navbar>
-</my-navbar>
->>>>>>> 6ec49f680d3290b8a98c6e2544eb0d9c2c1d96d9
 
 
 
